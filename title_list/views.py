2a30cca0d5374730a872b5f2f4545bf43b7f1f93
VaDKo61 <vadkoo@mail.ru>
2023-11-10 15:55:15 +0300

    edit search

--- title_list/views.py
+++ title_list/views.py
@@ -13,12 +13,16 @@ class Search(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(Search, self).get_context_data(**kwargs)
-        context['search'] = self.request.GET['search']
+        search = self.request.GET['search']
+        context['search'] = search
+        Game.objects.all().select_related('polygon').filter(id=1)
+        sunday_games = Game.objects.get(polygon__iexact=search)
+        context['sunday_games'] = sunday_games
         return context
 
 
-
 def main_page(request):
+    """Select team, polygons and games"""
     data = {'teams': Team.objects.order_by('?')[:6],
             'polygons': Polygons.objects.order_by('?')[:6],
             'games': Game.objects.filter(is_future=True).order_by('date')[:2],
