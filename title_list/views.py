c24c3bb6f230d003c9df66aadbe476c00afe3be3
VaDKo61 <vadkoo@mail.ru>
2023-11-17 10:44:53 +0300

    finish searh

--- title_list/views.py
+++ title_list/views.py
@@ -16,12 +16,22 @@ class Search(ListView):
         context = super(Search, self).get_context_data(**kwargs)
         search = self.request.GET['search']
         context['search'] = search
-        # Add in html sunday game matching search
+        # Add in html sunday games matching search
         search_int = int(search) if isinstance(search, int) else search
         sunday_games = Game.objects.select_related('polygon').filter(
             Q(polygon__name__iregex=search) | Q(date__contains=search_int) |
             Q(organizer__contains=search)).order_by('date')
-        context['sunday_games'] = sunday_games
+        sunday_games_future = sunday_games.filter(is_future=True)
+        context['sunday_games_future'] = sunday_games_future
+        sunday_games_archive = sunday_games.filter(is_future=False)
+        context['sunday_games_archive'] = sunday_games_archive
+        # Add in html polygons matching search
+        polygons = Polygons.objects.filter(name__iregex=search)
+        context['polygons'] = polygons
+        # Add in html teams matching search
+        teams = Team.objects.filter(name__iregex=search)
+        context['teams'] = teams
+        context['search_none'] = True if sunday_games or polygons or teams else False
         return context
 
 
